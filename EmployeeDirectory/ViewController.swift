b6bf2df7c937c875c3650e03c081cf042b373e70
tildraev <amohajer45@gmail.com>
2022-12-15 15:15:53 -0700

    Initial Commit

---
+++ EmployeeDirectory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmployeeDirectory
+//
+//  Created by Arian Mohajer on 12/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
